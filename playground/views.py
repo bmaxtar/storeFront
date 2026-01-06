ab66d67dfd727781d659a1a77fc8fb6a382027e6
Maxtar <bmaxtar@gmail.com>
2026-01-06 15:54:23 +0000

    Ajout des fichiers initiaux pour les applications 'store' et 'tags', y compris les modèles, les vues et les tests.

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x = 5
+    y = 10
+    return x
 # Create your views here.
 def say_hello(request):
-    p=1
-    t=2
+    x = calculate()
     return render(request, 'playground.html', {'name': 'Maxtar Ov'})
